2418ece76cc2888f1be91bf01b894f91f5c54dd4
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-20 16:12:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(str1, str2) {
+  if (str1.length > 0 && str2.length > 0) {
+    return str1[0].toUpperCase() + str2[0].toUpperCase();
+  } else {
+    return '';
+  }
 }
 
 // Do not change the code below.
